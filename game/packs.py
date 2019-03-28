f0d8e406ccbd2ff89a8d7b1936c29452ebd936a2
sennoma <sempereon@protonmail.ch>
2019-03-27 21:46:37 -0700

    Added a bunch of (yet untested) levels

--- game/packs.py
+++ game/packs.py
@@ -1,8 +1,8 @@
 level_packs = [
     ("one", ["level0", "level1", "level2", "level3", "level4", "level5"]),
-    ("two", ["level6", "level7", "level8"]),
-    ("three", []),
-    ("four", []),
+    ("two", ["level6", "level7", "level8", "level9", "level10"]),
+    ("three", ["level1", "level2", "level13", "level14", "level15"]),
+    ("four", ["level16", "leve17", "level8"]),
     ("five", []),
     ("six", []),
 ]
