82c363a4baf0881965e8bb0572cb3bc1bee52523
sennoma <sempereon@protonmail.ch>
2019-03-28 21:43:18 -0700

    Modified existing levels with new puzzle mechanics

--- game/packs.py
+++ game/packs.py
@@ -1,8 +1,8 @@
 level_packs = [
     ("one", ["level0", "level1", "level2", "level3", "level4", "level5"]),
-    ("two", ["level6", "level7", "level8", "level9", "level10"]),
-    ("three", ["level1", "level2", "level13", "level14", "level15"]),
-    ("four", ["level16", "leve17", "level8"]),
+    ("two", ["level6", "level7", "level8", "level9", "level10", "level11"]),
+    ("three", ["level12", "level13", "level14", "level15", "level16", "level17"]),
+    ("four", ["level18"]),
     ("five", []),
     ("six", []),
 ]
