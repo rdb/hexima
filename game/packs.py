9990224f223a929af7a1a404b01cf9b742c1785b
rdb <git@rdb.name>
2019-03-30 18:59:41 +0100

    more level work

--- game/packs.py
+++ game/packs.py
@@ -1,6 +1,6 @@
 level_packs = [
-    ("one", ["intro", "level3", "level5", "level0", "level1", "level14"]),
-    ("two", ["level6", "level7", "level8", "level9", "level10", "level11"]),
+    ("one", ["intro", "level0", "level1", "level2", "level3", "level5"]),
+    ("two", ["level19", "level4", "level6", "level7", "level8", "level11"]),
     ("three", ["level12", "level13", "level14", "level15", "level16", "level17"]),
     ("four", ["level18", "level19", "level20", "level21", "level22", "level23"]),
     ("five", ["level24", "level25", "level26", "level27", "level28", "level29"]),
