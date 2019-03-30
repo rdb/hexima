591ef170645ce428193eeeacf398b8176fe7ffcd
sennoma <sempereon@protonmail.ch>
2019-03-30 13:42:05 -0700

    Fixed a typo

--- game/packs.py
+++ game/packs.py
@@ -2,7 +2,7 @@ level_packs = [
     ("one", ["intro", "level0", "level1", "level3", "level18", "level5"]),
     ("two", ["level33", "level20", "level19", "level21", "level35", "level16"]),
     ("three", ["level2", "level14", "level6", "level25", "level32", "level8"]),
-    ("four", ["level15", "level10", "leve22", "level12", "level4", "level13"]),
+    ("four", ["level15", "level10", "level22", "level12", "level4", "level13"]),
     ("five", ["level11", "level31", "level17", "level7", "level34", "level9"]),
     ("six", ["level26", "level24", "level28", "level30", "level27", "level29"]),
 ]
