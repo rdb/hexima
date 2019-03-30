3284b68a3ca9bca499033b817d720530fa4e3554
sennoma <sempereon@protonmail.ch>
2019-03-29 21:38:57 -0700

    Added the rest of the levels; playtested and set par on a few

--- game/packs.py
+++ game/packs.py
@@ -2,7 +2,7 @@ level_packs = [
     ("one", ["level0", "level1", "level2", "level3", "level4", "level5"]),
     ("two", ["level6", "level7", "level8", "level9", "level10", "level11"]),
     ("three", ["level12", "level13", "level14", "level15", "level16", "level17"]),
-    ("four", ["level18"]),
-    ("five", []),
-    ("six", []),
+    ("four", ["level18", "level19", "level20", "level21", "level22", "level23"]),
+    ("five", ["level24", "level25", "level26", "level27", "level28", "level29"]),
+    ("six", ["level30", "level31", "level32", "level33", "level34", "level35"]),
 ]
