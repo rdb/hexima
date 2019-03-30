a2548d805c12faae430b5038a73b2b7ed3e383a3
rdb <git@rdb.name>
2019-03-30 17:54:11 +0100

    Level fixes and reorganisation

--- game/packs.py
+++ game/packs.py
@@ -1,5 +1,5 @@
 level_packs = [
-    ("one", ["level0", "level1", "level2", "level3", "level4", "level5"]),
+    ("one", ["intro", "level3", "level5", "level0", "level1", "level14"]),
     ("two", ["level6", "level7", "level8", "level9", "level10", "level11"]),
     ("three", ["level12", "level13", "level14", "level15", "level16", "level17"]),
     ("four", ["level18", "level19", "level20", "level21", "level22", "level23"]),
