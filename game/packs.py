af843e4f6f747feb70ea1d1158c7d010e60ddb5d
sennoma <sempereon@protonmail.ch>
2019-03-30 15:02:19 -0700

    Added remaining missing par scores; fixed some broken levels

--- game/packs.py
+++ game/packs.py
@@ -1,6 +1,6 @@
 level_packs = [
-    ("one", ["intro", "level0", "level1", "level3", "level18", "level5"]),
-    ("two", ["level33", "level20", "level19", "level21", "level35", "level16"]),
+    ("one", ["intro", "level0", "level1", "level3", "level35", "level5"]),
+    ("two", ["level33", "level20", "level19", "level21", "level18", "level16"]),
     ("three", ["level2", "level14", "level6", "level25", "level32", "level8"]),
     ("four", ["level15", "level10", "level22", "level12", "level4", "level13"]),
     ("five", ["level11", "level31", "level17", "level7", "level34", "level9"]),
